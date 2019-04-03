9f7380e5968e5ab691ced63ab9ca7b79008f7c0b
denibulkashvili <denibulkashvili@gmail.com>
2019-04-04 01:54:09 +0700

    Add models: * Lesson * Tag

--- lesson_plans/models.py
+++ lesson_plans/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Lesson(models.Model):
+    """Creates a Lesson model"""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='lesson title',
+    )
+    tags = models.ManyToManyField(
+        'Tag', 
+        related_name="lessons",
+        related_query_name='lesson',
+    )
+
+    class Meta: 
+        indexes = [models.Index(fields=['title'])]
+        ordering = ['-title']
+        verbose_name = 'lesson'
+        verbose_name_plural = 'lessons'
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('lesson_detail', args=[str(self.id)]) # or kwargs={"pk": self.pk}
+
+    # example url
+    # from news import views
+    # path('archive/', views.archive, name='news-archive')
+
+class Tag(models.Model):
+    """Creates a Tag model"""
+    name = models.CharField(
+        max_length=20,
+        verbose_name='tag name',
+    )
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('tag_detail', args=[str(self.id)])

b78c434ba33ae202c9fc3770c02173c429fa3f66
denibulkashvili <denibulkashvili@gmail.com>
2019-04-05 01:32:31 +0700

    Add tests for Lesson model

---
+++ lesson_plans/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from lesson_plans.models import Lesson, Tag
+
+# Create your tests here.
+class LessonTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        Lesson.objects.create(title='Food lesson')
+    
+    # def setUp(self):
+    #     print("setUp: Run once for every test method to setup clean data.")
+    #     pass
+
+    def test_title_label(self):
+        lesson = Lesson.objects.get(id=1)
+        field_label = lesson._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'lesson title')
+
+    def test_title_max_length(self):
+        lesson = Lesson.objects.get(id=1)
+        max_length = lesson._meta.get_field('title').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_object_name_is_title(self):
+        lesson = Lesson.objects.get(id=1)
+        expected_object_name = lesson.title
+        self.assertEquals(expected_object_name, str(lesson))
+
+    # def test_get_absolute_url(self):
+    #     lesson = Lesson.objects.get(id=1)
+    #     # This will also fail if the urlconf is not defined.
+    #     self.assertEquals(lesson.get_absolute_url(), 'lesson/1')

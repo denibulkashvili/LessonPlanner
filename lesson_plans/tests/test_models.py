e56b8a08ce942a488b6874f34d45a79693f6503e
denibulkashvili <denibulkashvili@gmail.com>
2019-04-05 10:29:00 +0700

    Add tests for Tag model

--- lesson_plans/tests/test_models.py
+++ lesson_plans/tests/test_models.py
@@ -31,3 +31,24 @@ class LessonTestCase(TestCase):
     #     lesson = Lesson.objects.get(id=1)
     #     # This will also fail if the urlconf is not defined.
     #     self.assertEquals(lesson.get_absolute_url(), 'lesson/1')
+
+class TagTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        Tag.objects.create(name='food')
+
+    def test_name_label(self):
+        tag = Tag.objects.get(id=1)
+        field_label = tag._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'tag name')
+
+    def test_name_max_length(self):
+        tag = Tag.objects.get(id=1)
+        max_length = tag._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20)
+
+    def test_object_name_is_name(self):
+        tag = Tag.objects.get(id=1)
+        expected_name = tag.name
+        self.assertEquals(expected_name, str(tag))

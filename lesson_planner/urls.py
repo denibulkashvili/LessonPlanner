49e4186fb5cf6369f9b8a531354342306c98c43a
denibulkashvili <denibulkashvili@gmail.com>
2019-04-10 14:38:46 +0700

    Change admin url

--- lesson_planner/urls.py
+++ lesson_planner/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("lesson_plans.urls"))]
+urlpatterns = [path("laoban/", admin.site.urls), path("", include("lesson_plans.urls"))]

fa9fa6d3b8a1e5187346284890e889e7aae8f596
denibulkashvili <denibulkashvili@gmail.com>
2019-04-11 09:40:55 +0700

    Update settings

--- lesson_planner/settings.py
+++ lesson_planner/settings.py
@@ -133,6 +133,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Configure Django App for Heroku.
 import django_heroku

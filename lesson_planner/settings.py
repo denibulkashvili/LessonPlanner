138831a2a2f8840bb6db446eff9afcb942bc4cbc
denibulkashvili <denibulkashvili@gmail.com>
2019-04-11 09:32:23 +0700

    Update settings

--- lesson_planner/settings.py
+++ lesson_planner/settings.py
@@ -90,11 +90,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        'CONN_MAX_AGE': 500,
     }
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

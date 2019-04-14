80bbb92a78e0c264a613b8955993cba876daf3a1
denibulkashvili <denibulkashvili@gmail.com>
2019-04-14 22:10:11 +0700

    Add more lesson detail tests

--- lesson_plans/tests/test_views.py
+++ lesson_plans/tests/test_views.py
@@ -44,11 +44,58 @@ class LessonDetailPageTests(TestCase):
     """Test LessonDetail page"""
 
     def setUp(self):
-        self.lesson = Lesson.objects.create(title="Countries Lesson")
+        self.lesson = Lesson.objects.create(
+            title="Countries Lesson",  
+            book="Get Smart 5", 
+            lesson_number=1, 
+            lesson_duration=90,
+            lesson_objectives="Learn names of the countries.", 
+            resources="Books, map, flashcards.", 
+            content="Learn the vocabulary. Sing the song about countries. Write the names of the ountries on the map.", 
+            video_link="https://www.youtube.com/watch?v=4gHbPDdGCFs"
+        )
+        self.tag = Tag.objects.create(name="countries")
+        self.lesson.tags.add(self.tag)
+        
 
     def test_lesson_detail_page_template(self):
         response = self.client.get(f"/lesson/{self.lesson.id}/")
         self.assertEqual(response.status_code, 200)
+
+    def test_lesson_detail_page_displays_tags(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "countries")
+
+    def test_lesson_detail_page_displays_book(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Get Smart 5")
+
+    def test_lesson_detail_page_displays_lesson_number(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Lesson number: 1")
+
+    def test_lesson_detail_page_displays_lesson_duration(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Lesson duration: 90 minutes.")
+
+    def test_lesson_detail_page_displays_lesson_resources(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Resources: Books, map, flashcards.")
+    
+    def test_lesson_detail_page_displays_lesson_objectives(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Objectives: Learn names of the countries.")
+    
+    def test_lesson_detail_page_displays_lesson_content(self):
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, "Content: Learn the vocabulary.")
+
+    def test_lesson_detail_page_displays_video_link(self):
+        iframe_scr_tag = 'src="https://www.youtube.com/embed/4gHbPDdGCFs">'
+        response = self.client.get("/lesson/1/")
+        self.assertContains(response, iframe_scr_tag)
+
+    
 
 class TagListPageTests(TestCase):
     """Test TagList page"""
